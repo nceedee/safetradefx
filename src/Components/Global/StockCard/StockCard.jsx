783a0b561503bd2f72d9a53e1afe2a5faa8766f7
nceedee <nnajidavid58@gmail.com>
2024-08-07 18:53:24 +0100

    done

--- src/Components/Global/StockCard/StockCard.jsx
+++ src/Components/Global/StockCard/StockCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const StockCard = ({ head, text, btn, image }) => {
   return (
-    <div className="bg-white rounded-md w-[300px] shadow-lg flex flex-col space-y-3 p-4 text-gray-700">
+    <div className="bg-white rounded-md lg:w-[300px] w-[90%] m-auto shadow-lg flex flex-col space-y-3 p-4 text-gray-700">
       <h1 className="font-bold text-2xl">{head}</h1>
       <p>{text}</p>
       <button
