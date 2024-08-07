94091fc818dca8f2af7e36c12c3ce7de18b1f148
nceedee <nnajidavid58@gmail.com>
2024-08-07 20:44:56 +0100

    done

--- src/Components/Layout/LandingPageLayout/RateDashboard/RateDashboard.jsx
+++ src/Components/Layout/LandingPageLayout/RateDashboard/RateDashboard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const RateDashboard = () => {
   return (
     <div>
-      <div className="m-auto py-16 scrollbar-hidden border-t ">
+      <div className="m-auto py-16 scrollbar-hidden  border-t ">
         <iframe
-          className="w-[80%] m-auto scrollbar-hidden h-[100vh] text-white"
+          className="w-[70%] lg:w-[80%] m-auto scrollbar-hidden h-[100vh] text-white"
           src="https://widget.coinlib.io/widget?type=full_v2&theme=dark&pref_coin_id=1505&cnt=10&graph=yes"
           frameBorder="0"
           style={{ overflow: "hidden" }}
