c62e88c1914f3da89ef8a568451062572a257ce8
nceedee <nnajidavid58@gmail.com>
2024-08-10 16:36:56 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,8 @@ import {
   useNavigationType,
 } from "react-router-dom";
 import LandingPage from "./Components/Pages/LandingPage/LandingPage";
-import About from "./Components/Pages/LandingPage/About/About";
-import Plans from "./Components/Pages/LandingPage/Plans/Plans";
-import Support from "./Components/Pages/LandingPage/Support/Support";
 import Signup from "./Components/Pages/LandingPage/Signup/Signup";
 import Login from "./Components/Pages/LandingPage/Login/Login";
-import Services from "./Components/Pages/LandingPage/Service/Services";
 import Dashboard from "./Components/Pages/Dashboard/Dashboard";
 import { useAuth } from "./Components/Global/hook/useAuth";
 import LoadingModal from "./Components/Global/LoadingModal/LoadingModal";
@@ -49,10 +45,6 @@ const AppContent = () => {
       {isLoading && <LoadingModal />}
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/about-us" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/plans" element={<Plans />} />
-        <Route path="/support" element={<Support />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route
