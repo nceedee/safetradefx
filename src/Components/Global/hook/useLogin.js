2fc94912e5b80b5b7466d3b2c7b999f3abfac644
N Cee Dee Tech <102691454+nceedee@users.noreply.github.com>
2024-08-13 09:33:16 +0100

    Update useLogin.js

--- src/Components/Global/hook/useLogin.js
+++ src/Components/Global/hook/useLogin.js
@@ -2,16 +2,12 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../Context/AuthContext"; 
-import { auth } from "../../config/firebase"; 
+import { AuthContext } from "../../Context/AuthContext";
+import { auth } from "../../config/firebase";
 
 export const useLogin = () => {
   const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const [message, setMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const { dispatch } = useContext(AuthContext);
@@ -19,21 +15,15 @@ export const useLogin = () => {
   const onSubmit = async (data) => {
     try {
       setIsLoading(true);
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        data.email,
-        data.password
-      );
+      const userCredential = await signInWithEmailAndPassword(auth, data.email, data.password);
       const user = {
         id: userCredential.user.uid,
         name: userCredential.user.displayName || "",
         email: userCredential.user.email || "",
-        password: "", // Typically you wouldn't include the password in the user object
       };
 
       dispatch({ type: "LOGIN", payload: user });
       navigate("/dashboard");
-      
     } catch (error) {
       setMessage(error.message);
       setIsLoading(false);
