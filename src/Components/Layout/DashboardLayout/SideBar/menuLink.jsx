72af22db9dc8815dc444bb3a068175a7624b6392
N Cee Dee Tech <102691454+nceedee@users.noreply.github.com>
2024-10-06 20:39:53 +0100

    Update menuLink.jsx

--- src/Components/Layout/DashboardLayout/SideBar/menuLink.jsx
+++ src/Components/Layout/DashboardLayout/SideBar/menuLink.jsx
@@ -6,6 +6,7 @@ import { BiMoneyWithdraw } from "react-icons/bi";
 import { TbClockDollar } from "react-icons/tb";
 import { RiWallet2Fill } from "react-icons/ri";
 import { FaPeoplePulling } from "react-icons/fa6";
+import { PiHandDepositFill } from "react-icons/pi";
 
 import { BsFillGearFill } from "react-icons/bs";
 
@@ -13,7 +14,7 @@ export const menuLink = [
     {text: 'Account' ,icon: <MdAccountCircle/>, path: '/my-account'},
     {text: 'Invest' ,icon: <FaHandHoldingDollar/>, path: '/invest'},
     {text: 'My Deposits' ,icon: <RiLuggageDepositFill/>, path: '/my-deposits'},
-    {text: 'Reinvest' ,icon: <RiExchangeDollarLine/>, path: '/reinvest'},
+    {text: 'Deposit' ,icon: <PiHandDepositFill />, path: '/deposit'},
     {text: 'Withdraw' ,icon: <BiMoneyWithdraw/>, path: '/withdraw'},
     {text: 'Transaction History' ,icon: <TbClockDollar/>, path: '/transaction-history'},
     {text: 'Wallets' ,icon: <RiWallet2Fill/>, path: '/wallets'},
